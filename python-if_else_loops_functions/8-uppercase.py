27d05ad2736e1f9700ce25f3b600cb691fe68614
Kazim <kazimammadli@gmail.com>
2025-06-12 22:02:11 +0000

    8.task

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            char = chr(ord(char) - 32)
+            new_str += char
+        else:
+            new_str += char
+    return new_str
