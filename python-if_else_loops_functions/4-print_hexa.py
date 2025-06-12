27f40d756900c453fb7b8d6beb06c91b252697fe
Kazim <kazimammadli@gmail.com>
2025-06-12 23:19:09 +0400

    4.task

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 str = ""
 for i in range(99):
     str += f"{i} = {hex(i)}\n"
-#print("{}".format(str.rstrip()))
-print("{}".format(str), end="")
+print("{}".format(str.rstrip()))
+#("{}".format(str), end="")
