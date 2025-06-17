5dc7fab7c064a3aac88dd3662d9719b47159214e
Kazim <11111@holbertonstudents.com>
2025-06-17 19:33:53 +0400

    2.task-fixed-totally

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-if __name__ =="__main__":
+if __name__ == "__main__":
     n = len(sys.argv ) - 1
     if n > 1:
         print("{} arguments:".format(n))
@@ -9,4 +9,4 @@ if __name__ =="__main__":
     elif n == 1:
         print("{} argument:\n1: {}".format(n, sys.argv[1]))
     else:
-        print("0 arguments".format())
+        print("0 arguments.".format())
