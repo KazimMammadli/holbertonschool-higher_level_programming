cb6dff5949383aeccf39fae8a17d8e367ae2013f
Kazim <kazimammadli@gmail.com>
2025-06-18 17:01:27 +0400

    3.task

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-#additional method 
-"""def common_elements2(set_1, set_2):
-    new_set = set()
-    new_set=set_1.intersection(set_2)
-    return new_set"""
 def common_elements(set_1, set_2):
     new_set = set()
     for i in set_1:
