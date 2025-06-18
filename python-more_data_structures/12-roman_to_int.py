af7d58a32892b7eed2f5b81674cb05f9de32ab15
Kazim <kazimammadli@gmail.com>
2025-06-18 18:55:55 +0400

    12.task

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,14 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str:
         return 0
-    r_dict = {"I": 1,
-             "V": 5,
-             "X": 10,
-             "L": 50,
-             "C": 100,
-             "D": 500,
-             "M": 1000}
+    r_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000}
     sum = 0
     for i in range(len(roman_string) - 1):
         if r_dict[roman_string[i]] >= r_dict[roman_string[i + 1]]:
