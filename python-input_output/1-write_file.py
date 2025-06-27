92654d722e83781f053bcd9a77882a80bd2dc7c5
Kazim <kazimammadli@gmail.com>
2025-06-28 00:07:05 +0400

    1.task

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module is used to write text to the file."""
+
+
+def write_file(filename="", text=""):
+    """Write text to the file and 
+    return the number of characters."""
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
