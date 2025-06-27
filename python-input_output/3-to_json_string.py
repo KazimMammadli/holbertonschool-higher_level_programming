0c50f3b6a59484f721dd21fdd9f6d56057277e02
Kazim <kazimammadli@gmail.com>
2025-06-28 00:39:22 +0400

    3.task

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""This module is about serialization."""
 import json
-"""This module is about serialization"""
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation of an object"""
+    """Return the JSON representation of an object."""
     return json.dumps(my_obj)
