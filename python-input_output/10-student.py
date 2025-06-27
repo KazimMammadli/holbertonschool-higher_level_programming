64694937cff48d316bdb42491b19dd06711482fb
Kazim <kazimammadli@gmail.com>
2025-06-28 02:05:18 +0400

    10.task

---
+++ python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Student to disk and reload."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name, last_name, age):
+        """Init constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dictionary for json"""
+        dictionary = self.__dict__
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {key: dictionary[key] for key in attrs if key in dictionary}
+        return dictionary
