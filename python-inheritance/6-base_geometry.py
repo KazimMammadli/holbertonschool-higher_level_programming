795fe3a5e75ee2e0b0c64ccdf9bc7d0f47e2046b
Kazim <kazimammadli@gmail.com>
2025-06-27 16:00:28 +0400

    6.task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class"""
+
+
+class BaseGeometry:
+    """Define area function"""
+
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
