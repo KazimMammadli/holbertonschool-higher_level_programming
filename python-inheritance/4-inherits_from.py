9ba264bb125686704c1543f7a5324a66cf21b354
Kazim <kazimammadli@gmail.com>
2025-06-27 15:42:35 +0400

    4.task

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a function"""
+
+
+def inherits_from(obj, a_class):
+    """Return True if the object is an instance of a class that inherited
+      (directly or indirectly) from the specified class ; otherwise False"""
+    return issubclass(obj, a_class)
