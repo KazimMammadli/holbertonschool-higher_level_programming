601f4802039ec252ed633213754b1b216e737425
Kazim <kazimammadli@gmail.com>
2025-06-28 00:00:32 +0400

    0.task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This file is used to read another file."""
+
+
 def read_file(filename=""):
+    """Print the contents of file."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read())
