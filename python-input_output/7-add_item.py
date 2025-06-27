145e84310346aaecf76cd724f7d38dbfb9d43473
Kazim <kazimammadli@gmail.com>
2025-06-28 01:25:40 +0400

    7.task

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+"""Load, add, save"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+filename = "add_item.json"
+try:
+    info = load_from_json_file(filename)
+except FileNotFoundError:
+    info = []
+info[:] += sys.argv[1:]
+save_to_json_file(info, filename)
