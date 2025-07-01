634b61e97292f005579c1d91f53a3f59a17b66f4
Kazim <kazimammadli@gmail.com>
2025-07-01 15:14:12 +0400

    1.task

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -17,15 +17,22 @@ class CustomObject:
 
     def serialize(self, filename):
         """Serialize the file."""
-        with open(filename, "wb") as f:
-            pickle.dump(self,f)
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self,f)
+        except Exception:
+            print(f"Error: File '{filename}' not found.")
 
     @classmethod
     def deserialize(cls, filename):
         """Deserialize the file."""
-        with open(filename, "rb") as f:
-            return pickle.load(f)
-    
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            print(f"Error: File '{filename}' not found.")
+            return None 
+
     def display(self):
         """Display the contents."""
         print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")    
